27a57e26a74f4284b3a38de16b58af7b7800e3a4
Evan Kahn <ekahn@hmc.edu>
2017-01-22 13:22:31 -0800

    Added actual pin assignments.

---
+++ config.h
@@ -0,0 +1,11 @@
+#define STEP_MS 500
+
+#define NUM_INPUTS 3
+#define NUM_OUTPUTS 16
+#define NUM_POWERS 6
+#define NUM_GROUNDS 7
+
+const uint8_t powers[] = {P1, P11, P13, P15, P37, P39};
+const uint8_t grounds[] = {P40, P38, P36, P10, P12, P14, P16};
+const uint8_t inputs[] = {P8, P9, P3}; // clk1, clk2, reset
+const uint8_t outputs[] = {P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32, P33}; // anodes, then cathodes
--- tct.ino
+++ tct.ino
@@ -1,40 +1,16 @@
 #include "pins.h"
 #include "data.h"
-
-// BEGIN USER DEFINITIONS
-#define PWR P1
-#define GND P1
-#define STEP_MS 500
-#define NUM_INPUTS 3
-#define NUM_OUTPUTS 16
-
-const uint8_t inputs[] = {P1, P2, P3}; // clk1, clk2, reset
-const uint8_t outputs[] = {P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20}; // anodes, then cathodes
-// END USER DEFINITIONS
+#include "config.h"
 
 void setup() {
   Serial.begin(9600);
-  
-  pinMode(PWR, OUTPUT);
-  pinMode(GND, OUTPUT);
-  digitalWrite(PWR, 0);
-  digitalWrite(GND, 0);
 
   //general purpose iterators
   uint16_t i = 0;
   unsigned long dt = 0;
   
-  while (i < NUM_INPUTS){
-    pinMode(inputs[i], INPUT);
-    ++i;
-  }
-  i = 0;
-  while (i < NUM_OUTPUTS){
-    pinMode(outputs[i], OUTPUT);
-    digitalWrite(outputs[i], 0);
-    ++i;
-  }
-  i = 0;
+  init_pins();
+  
   delay(300);
   uint16_t j = 0;
   
@@ -47,7 +23,7 @@ void setup() {
     dt = millis() + STEP_MS;
     Serial.print("Inputs: ");
     while (j < NUM_INPUTS){
-      uint16_t a = (i * NUM_INPUTS+j) >> 3;
+      uint16_t a = (i * NUM_INPUTS + j) >> 3;
       digitalWrite(inputs[j], input_vals[a] & inp_mask);
       Serial.print((input_vals[a] & inp_mask) ? "1" : "0");
       inp_mask = inp_mask >> 1;
@@ -55,23 +31,51 @@ void setup() {
       ++j;
     }
     Serial.println();
+    
     j = 0;
     
     while (millis() < dt) {}
     
-    Serial.print("Outputs: ");
+    Serial.print("Expected outputs: ");
     while (j < NUM_OUTPUTS){
-      uint16_t a = (i * NUM_OUTPUTS+j) >> 3;
-//      digitalWrite(outputs[j], output_vals[a] & out_mask);
+      uint16_t a = (i * NUM_OUTPUTS + j) >> 3;
       Serial.print((output_vals[a] & out_mask) ? "1" : "0");
       out_mask = out_mask >> 1;
       if (out_mask == 0) out_mask = 0x80;
+      Serial.print("Outputs determined: ");
+
       ++j;
     }
     Serial.println();
     j = 0;
     ++i;
   }
+}
+
+void init_pins(){
+  uint8_t i = 0;
+  while (i < NUM_INPUTS){
+    pinMode(inputs[i], INPUT);
+    ++i;
+  }
+  i = 0;
+  while (i < NUM_OUTPUTS){
+    pinMode(outputs[i], OUTPUT);
+    digitalWrite(outputs[i], 0);
+    ++i;
+  }
+  i = 0;
+  while (i < NUM_GROUNDS){
+    pinMode(grounds[i], OUTPUT);
+    digitalWrite(grounds[i], 0);
+    ++i;
+  }
+  i = 0;
+  while (i < NUM_POWERS){
+    pinMode(powers[i], OUTPUT);
+    digitalWrite(powers[i], 1);
+  }
+  i = 0;
 }
 
 void loop(){}
