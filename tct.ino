a38bd240f033107da94c6c5163603a4e7b0c5760
Evan Kahn <ekahn@hmc.edu>
2017-01-23 15:55:38 -0800

    preliminary testbench complete

--- tct.ino
+++ tct.ino
@@ -2,6 +2,8 @@
 #include "data.h"
 #include "config.h"
 
+
+
 void setup() {
   Serial.begin(9600);
 
@@ -12,44 +14,70 @@ void setup() {
   init_pins();
   
   delay(300);
-  uint16_t j = 0;
   
   uint8_t inp_mask = 0x80;
   uint8_t out_mask = 0x80;
+  uint32_t err_count = 0;
   
   while (i < NUM_STEPS){
-    Serial.print("Step: ");
-    Serial.println(i);
-    dt = millis() + STEP_MS;
-    Serial.print("Inputs: ");
+    
+    byte read_outputs[NUM_OUTPUTS];
+    byte check_outputs[NUM_OUTPUTS];
+    byte read_inputs[NUM_INPUTS];
+    
+    uint32_t j = 0;
+    
     while (j < NUM_INPUTS){
       uint16_t a = (i * NUM_INPUTS + j) >> 3;
       digitalWrite(inputs[j], input_vals[a] & inp_mask);
-      Serial.print((input_vals[a] & inp_mask) ? "1" : "0");
+      read_inputs[j] = input_vals[a] & inp_mask;
       inp_mask = inp_mask >> 1;
       if (inp_mask == 0) inp_mask = 0x80;
       ++j;
     }
-    Serial.println();
     
     j = 0;
     
     while (millis() < dt) {}
+    dt = millis() + STEP_MS;
     
-    Serial.print("Expected outputs: ");
+    byte check = 0;
+    
     while (j < NUM_OUTPUTS){
       uint16_t a = (i * NUM_OUTPUTS + j) >> 3;
-      Serial.print((output_vals[a] & out_mask) ? "1" : "0");
+      check_outputs[j] = output_vals[a] & out_mask;
       out_mask = out_mask >> 1;
       if (out_mask == 0) out_mask = 0x80;
-      Serial.print("Outputs determined: ");
-
+      read_outputs[j] = digitalRead(outputs[j]);
+      if (read_outputs[j] != check_outputs[j]) check = 1;
       ++j;
     }
-    Serial.println();
     j = 0;
+    
+    if (check){
+      Serial.print("Discrepancy on step: ");
+      Serial.print(i);
+      Serial.println(".");
+      Serial.print("Inputs: ");
+      while (j < NUM_INPUTS){
+        Serial.print(read_inputs[j]);
+      }
+      Serial.print("\nExpected outputs: ");
+      while (j < NUM_OUTPUTS){
+        Serial.print(read_outputs[j]);
+      }
+      Serial.print("\nActual outputs:   ");
+      while (j < NUM_OUTPUTS){
+        Serial.print(check_outputs[j]);
+      }
+      Serial.println();
+      ++err_count;
+    }
     ++i;
   }
+  Serial.print("Test completed with ");
+  Serial.print(err_count);
+  Serial.println(" errors.");
 }
 
 void init_pins(){
